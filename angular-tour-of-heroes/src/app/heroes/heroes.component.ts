b7fbcb7fb5dfd63237926fb2930dd72cf627e204
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-09-21 11:03:18 +0200

    Added messages when users click on a hero

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,13 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,
+    private messageService: MessageService) { }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: Selected Hero id='+hero.id);
   }
 
   ngOnInit(): void {
